e4811f752c88690fb31a0144aa4920f279871ace
Danilo Araujo <danilocostafp@gmail.com>
2025-02-08 20:00:48 -0300

    Subindo o projeto calculadora com ReactJs

---
+++ src/components/Input/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    display: flex;
+    align-items: center;
+    
+    width: 100%;
+    height: 75px;
+    background-color: #AFAFFF;
+    
+    font-size: 24px;
+    font-family: "Roboto", sans-serif;
+    border-radius: 25px;
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AFAFFF;
+        border: none;
+        border-radius: 10px 10px 0 0;
+        padding: 0 10px;
+        font-size: 24px;
+        text-align: right;
+        color: #000000;
+    }
+`;
